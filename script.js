a6aaa20fe11628ee78dba59bbe68d03ee70e2f3a
Elat <talebjorn@gmail.com>
2022-06-23 11:51:42 +0200

    Add basic functions and CSS styling

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const subtraction = (number) => number.reduce((a,b) => a - b);
 const multiplication = (number) => number.reduce((a,b) => a * b);
 
 const operate = function (operator, ...sum) {
-    //The function should take an operator and two numbers as arguments
-    //It should then call the chosen of the above functions on the numbers
     switch (true) {
         case (operator === addition) : addition(sum);
         break;
@@ -20,3 +18,81 @@ const operate = function (operator, ...sum) {
         break;
     };
 };
+
+let operator = '';
+
+let firstSum = 0;
+
+let secondSum = 0;
+
+const screen = document.getElementById('display');
+
+const numberDisplayer = document.createElement('p');
+    numberDisplayer.classList.add('clickedNum');
+
+const secondNumberDisplayer = document.createElement('p');
+    secondNumberDisplayer.classList.add('clickedNum');
+
+const operatorDisplayer = document.createElement('p');
+    operatorDisplayer.classList.add('clickedNum');
+
+
+ 
+const AC = document.getElementById('ac');
+    AC.addEventListener('click', e => {
+        numberDisplayer.textContent = '';
+        operatorDisplayer.textContent = '';
+});
+    
+const number = document.querySelectorAll('.number');
+    number.forEach(item => 
+    item.addEventListener('click', e => {
+        if (operator < 1) {
+       numberDisplayer.textContent += e.target.textContent;
+        firstSum += e.target.textContent;
+        console.log(firstSum)
+        return firstSum;
+        } else {
+            secondNumberDisplayer.textContent += e.target.textContent;
+            secondSum += e.target.textContent;
+            return secondSum;
+        }
+       
+}));
+
+const operators = document.querySelectorAll('.operator');
+   
+//     operators.forEach(item =>
+//          item.addEventListener('click', e => {
+//             operator++;
+//             operatorDisplayer.textContent = e.target.textContent;  
+//             operator = e.target.textContent;
+//             console.log(operator);
+//         return operator;
+         
+// })); 
+    
+
+
+const equals = document.getElementById('equal');
+    equals.addEventListener('click', e => {
+       
+        numberDisplayer.textContent = '';
+        operatorDisplayer.textContent = '';
+        secondNumberDisplayer.textContent = '';
+        operator--;
+        operator = e.target.textContent;
+        console.log(operator)
+        let total = operate(operator, firstSum);
+        numberDisplayer.textContent = total;
+        console.log(total)
+    
+});
+
+
+
+
+screen.appendChild(secondNumberDisplayer);
+screen.appendChild(operatorDisplayer);
+screen.appendChild(numberDisplayer);
+
